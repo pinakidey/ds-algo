2ba7786f85bd7974a532a1003d627a083ed051d9
Pinaki Dey <pinaki@pinakidey.com>
2021-02-07 15:00:32 +0900

    Cleanup

--- Leetcode/src/Draft.java
+++
@@ -1,9 +0,0 @@
-package Leetcode.src;
-
-public class Draft {
-    public static void main(String[] args) {
-        String[] names = new String[10];
-        names[1] = "Pinaki";
-        System.out.println(names.length);
-    }
-}
